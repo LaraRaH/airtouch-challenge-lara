9fd2bb5d2ed2fae4c218c96f63f3b2ba803e64e7
Lara SAF <lararabanoh@gmail.com>
2025-09-03 16:55:51 +0200

    feat(core app): login done

--- airtouch_challenge/airtouch_challenge/settings.py
+++ airtouch_challenge/airtouch_challenge/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core"
 ]
 
 MIDDLEWARE = [
@@ -57,10 +58,11 @@ ROOT_URLCONF = "airtouch_challenge.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [],   # o [BASE_DIR / "templates"] si quieres una carpeta global
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -127,3 +129,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "/"  # a dónde redirigir después de login
+LOGOUT_REDIRECT_URL = "login"
